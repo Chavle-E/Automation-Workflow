db29673667ab1d09d9ab38a2ed4cb8294f726f25
Chavle_E <chavleshviliguka@gmail.com>
2024-07-16 16:22:02 +0400

    Change authorization method

--- Payroll/main.py
+++ Payroll/main.py
@@ -25,14 +25,19 @@ headers_deel = {
 }
 
 headers_harvest = {
-    'Authorization': f'Bearer {HARVEST_API_KEY}',
-    'Harvest-Account-Id': HARVEST_ACC_ID,
+    'Harvest-Account-Id': f"{HARVEST_ACC_ID}",
+    'Authorization': f'Bearer {HARVEST_API_KEY}'
 }
 
+# Log the environment variables to ensure they are loaded
 logging.info(f"DEEL_API_KEY Loaded: {'Yes' if DEEL_API_KEY else 'No'}")
 logging.info(f"HARVEST_API_KEY Loaded: {'Yes' if HARVEST_API_KEY else 'No'}")
 logging.info(f"HARVEST_ACC_ID Loaded: {'Yes' if HARVEST_ACC_ID else 'No'}")
 logging.info(f"HARVEST_ACC_ID Value: {HARVEST_ACC_ID}")
+
+if not all([DEEL_API_KEY, HARVEST_API_KEY, HARVEST_ACC_ID]):
+    logging.error(f"Missing environment variables: DEEL_API_KEY: {DEEL_API_KEY}, HARVEST_API_KEY: {HARVEST_API_KEY}, HARVEST_ACC_ID: {HARVEST_ACC_ID}")
+    raise EnvironmentError("One or more environment variables are missing")
 
 
 def get_previous_semi_month_dates():
